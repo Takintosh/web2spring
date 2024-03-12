3a28e0b4a30d99d6e5b76d823fedae827a7bbeb0
Amilcar Santana <takintosh@gmail.com>
2024-03-12 16:47:30 -0300

    First Commit

---
+++ src/main/java/com/takintosh/web2spring/Web2springApplication.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takintosh/web2spring/Web2springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
